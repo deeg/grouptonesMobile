49a07f5cee21594c3196fb96422dec3b4eaa66b7
Matthew K <kehlmanm@gmail.com>
2013-04-20 17:45:29 -0400

    Restructuring list templates and adding musicians and projects

--- app.js
+++ app.js
@@ -88,7 +88,21 @@ server.get('/logout', function (req, res) {
 server.get('/events', function (req, res) {
   connection.query('SELECT * from events where event_state = "' + req.user[0].artist_state + '" limit 10', function(err, rows) {
     console.log(rows);
-    res.render('events.dust', {events : rows});
+    res.render('eventsList.dust', {list : rows});
+  });
+});
+
+server.get('/musicians', function (req, res) {
+  connection.query('SELECT * from artists_profile where artist_state = "' + req.user[0].artist_state + '" limit 10', function(err, rows) {
+    console.log(rows);
+    res.render('musicianList.dust', {list : rows});
+  });
+});
+
+server.get('/projects', function (req, res) {
+  connection.query('SELECT * from projects where proj_state = "' + req.user[0].artist_state + '" limit 10', function(err, rows) {
+    console.log(rows);
+    res.render('projectList.dust', {list : rows});
   });
 });
 
