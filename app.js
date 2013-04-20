cde6fb7c01160b00f4142c49e6210c6f41395d0e
Matthew K <kehlmanm@gmail.com>
2013-04-20 18:02:13 -0400

    moving routes to new file in /lib

--- app.js
+++ app.js
@@ -61,50 +61,11 @@ server.configure(function(){
     server.use(server.router);
 });
 
-server.get('/', function (req, res){
-    console.log(req.user);
-    var currentUser = {};
-    if (req.user) {
-      currentUser = req.user[0];
-    }
-    res.render('base.dust', currentUser);
-});
 
-server.get('/login', function (req, res) {
 
-  res.render('login.dust');
-});
 
-server.post('/login', passport.authenticate('local', { failureRedirect: '/login', failureFlash: false }),
-  function (req, res) {
-    res.redirect('/');
-});
+require('./lib/routes')(server, passport, connection);
 
-server.get('/logout', function (req, res) {
-  req.logout();
-  res.redirect('/');
-});
-
-server.get('/events', function (req, res) {
-  connection.query('SELECT * from events where event_state = "' + req.user[0].artist_state + '" limit 10', function(err, rows) {
-    console.log(rows);
-    res.render('eventsList.dust', {list : rows});
-  });
-});
-
-server.get('/musicians', function (req, res) {
-  connection.query('SELECT * from artists_profile where artist_state = "' + req.user[0].artist_state + '" limit 10', function(err, rows) {
-    console.log(rows);
-    res.render('musicianList.dust', {list : rows});
-  });
-});
-
-server.get('/projects', function (req, res) {
-  connection.query('SELECT * from projects where proj_state = "' + req.user[0].artist_state + '" limit 10', function(err, rows) {
-    console.log(rows);
-    res.render('projectList.dust', {list : rows});
-  });
-});
 
 server.listen(port);
 console.log('Listening on localhost:' + port );
