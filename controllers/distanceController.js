62a129a47b8e1d26b0579070c3e97086b77e8b6c
Matthew K <kehlmanm@gmail.com>
2013-07-11 19:44:55 -0400

    Trimming distance to 2 decimal places, adding MI suffix

--- controllers/distanceController.js
+++ controllers/distanceController.js
@@ -33,5 +33,6 @@ module.exports.calculateDistance = function (lat2, lon2, uLat, uLng) {
     var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
         Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    return (R * c) / 1.609344;
+    //Trim result to 2 decimal places
+    return ((R * c) / 1.609344).toFixed(2);
 }
