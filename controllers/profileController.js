bdff3e8014b8be789d184b36b8cc816301ee5ede
dan <dan@gornstein.com>
2013-08-05 17:56:48 -0400

    Adding languages to edit profile page

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -3,17 +3,19 @@ module.exports.saveProfile = function (req, res, connection) {
 
     console.log(req.body);
 
-    connection.query('UPDATE artists_profile SET ? WHERE id = ' + id, req.body, function(err, rows){
-        if(!err){
-            if(rows.length){
-                res.json(200);
+    connection.query('UPDATE artists_languages SET ? WHERE profile_id = ' + id, {profile_id: id, language: req.body.languages}, function(err, rows){
+        connection.query('UPDATE artists_profile SET ? WHERE id = ' + id, req.body, function(err, rowss){
+            if(!err){
+                if(rowss.length){
+                    res.json(200);
+                }else{
+                    res.json(500);
+                }
             }else{
                 res.json(500);
             }
-        }else{
-            res.json(500);
-        }
-    });
+        })
+    })
 }
 
 
