137d20bc0254a67ab56dc36696cf6c9924c223e5
dan <dan@gornstein.com>
2013-08-05 18:08:45 -0400

    fixing indenting, moving getProfileDetails out to function in profileController

--- controllers/detailController.js
+++ controllers/detailController.js
@@ -11,25 +11,24 @@ module.exports.artistDetail = function (req, res, connection) {
             _.each(rowss, function(element, index){
                 genereIdToNameMap[element.id] = element.genre;
             })
-               //Get artists like to play
-                connection.query('SELECT * from artists_profileltp_join where ?', {"profile_id": rows[0]["id"]}, function(error, ltpRows){
-                    if (err) console.error(err);
-                    if (error) console.error(error);
-                    var likesToPlayData = [];
-                    _.each(ltpRows, function(element, index){
-                        var likesToPlayEntry = {
-                            genre:genereIdToNameMap[element['genre_id']],
-                            details:element['details']
-                        }
-                        likesToPlayData.push(likesToPlayEntry)
-                    })
-                    console.log(likesToPlayData)
-
-                    res.render('musicianDetail.dust', {item : rows, likesToPlayData:likesToPlayData, currentUser: req.user[0]});
+           //Get artists like to play
+            connection.query('SELECT * from artists_profileltp_join where ?', {"profile_id": rows[0]["id"]}, function(error, ltpRows){
+                if (err) console.error(err);
+                if (error) console.error(error);
+                var likesToPlayData = [];
+                _.each(ltpRows, function(element, index){
+                    var likesToPlayEntry = {
+                        genre:genereIdToNameMap[element['genre_id']],
+                        details:element['details']
+                    }
+                    likesToPlayData.push(likesToPlayEntry)
                 })
+                console.log(likesToPlayData)
 
-            });
-        })
+                res.render('musicianDetail.dust', {item : rows, likesToPlayData:likesToPlayData, currentUser: req.user[0]});
+            })
+        });
+    })
 }
 
 
--- controllers/profileController.js
+++ controllers/profileController.js
@@ -18,4 +18,16 @@ module.exports.saveProfile = function (req, res, connection) {
     })
 }
 
+module.exports.getProfileDetails = function (req, res, connection){
+    connection.query('SELECT * from artists_profile where id=' + req.user[0].id, function(err, rows){
+        if (err) console.error(err);
+        connection.query('SELECT * from artists_languages where profile_id = ' + rows[0].id, function(err, rowss){
+            console.log(rowss[0])
+            console.log(rowss[0].language);
+            rows[0].languages = rowss[0].language;
+            res.render('profile.dust', {user: rows[0], currentUser: req.user[0]});
+        })
+    });
+}
+
 
