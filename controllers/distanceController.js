678ae4770167e2ef386ec893cb65931b3fbbb9e0
dan <dan@gornstein.com>
2013-08-11 15:03:57 -0400

    Removing entries with null distance

--- controllers/distanceController.js
+++ controllers/distanceController.js
@@ -8,10 +8,7 @@
 var _ = require("underscore");
 
 module.exports.calculateDistances = function (rows, uLat, uLng, latParam, lngParam) {
-    console.log(latParam)
-    console.log(lngParam)
     _.each(rows, function(element, index){
-        if(index == 0){console.log(element);}
         var distance = module.exports.calculateDistance(element[latParam], element[lngParam], uLat, uLng);
         element.distance = distance;
     })
@@ -20,12 +17,17 @@ module.exports.calculateDistances = function (rows, uLat, uLng, latParam, lngPar
 
     rows = _.first(rows, 20);
 
-    console.log(rows);
+    //Remove entries with null distance
+    rows = _.filter(rows, function(row){return row.distance != null})
 
     return rows;
 }
 
 module.exports.calculateDistance = function (lat2, lon2, uLat, uLng) {
+    if(lat2 == null || lon2 == null || uLat == null || uLng == null){
+        //Return null distance because they are missing Lat/Lng params
+        return null;
+    }
     lat2 = parseFloat(lat2);
     lon2 = parseFloat(lon2);
     uLat = parseFloat(uLat);
@@ -44,6 +46,7 @@ module.exports.calculateDistance = function (lat2, lon2, uLat, uLng) {
     var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
         Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var result = ((R * c) / 1.609344).toFixed(2);
     //Trim result to 2 decimal places
-    return ((R * c) / 1.609344).toFixed(2);
+    return result
 }
