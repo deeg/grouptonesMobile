59845da06426724791abde769ab15b1198dcd5da
Matthew K <kehlmanm@gmail.com>
2013-09-21 19:26:42 -0400

    Resizing picture on artist detail

--- static/js/ListPagination.js
+++ static/js/ListPagination.js
@@ -4,28 +4,23 @@
 
     $(document).ready(function(){
        $('#GetMoreRecords').click(function(){
-          console.log('clicked');
            $.ajax({
                url: document.location.pathname.substr(1) + '/paginate/' + index + window.location.search,
                dataType: 'json'
            })
                .done(function(results) {
                    index += 20;
-                   console.log( "success" );
                    var paginationString = '';
                    results.forEach(function(obj, idx, arr){
                        dust.render(document.location.pathname.substr(1) + 'Item', obj, function(err, out){
-                           console.log(out);
                            paginationString += out;
                        });
                    });
 
-                   console.log('is async?');
                    $('#ListView').append(paginationString).listview('refresh');
                })
-               .fail(function() {
-                   index += 20;
-                   console.log('fail')
+               .fail(function(err) {
+                    console.log(err);
                });
        });
     });
