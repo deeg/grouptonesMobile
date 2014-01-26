704330c2cf568b2c7356de97d5e5902d94a2be4d
Dan Gornstein <dan@gornstein.com>
2014-01-26 17:40:14 -0500

    Removing languages from edit profile section. Also removing calls to language table in profileController.js since we are no longer displaying it. Removing 'looking to play' from musicians details per DAGs request since it cannot be in edit profile

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -4,30 +4,26 @@ module.exports.saveProfile = function (req, res) {
     var id = req.param('id');
 
     console.log(req.body);
-    DAL.makeQuery({query: 'UPDATE artists_languages SET ? WHERE profile_id = ?', escapedValues : [{profile_id: id, language: req.body.languages}, id]}, function(err, rows){
-        DAL.makeQuery({query: 'UPDATE artists_profile SET ? WHERE id = ?', escapedValues : [req.body, id]}, function(err, rowss){
-            if(!err){
-                if(rowss.length){
-                    res.json(200);
-                }else{
-                    res.json(500);
-                }
+    DAL.makeQuery({query: 'UPDATE artists_profile SET ? WHERE id = ?', escapedValues : [req.body, id]}, function(err, rows){
+        if(!err){
+            if(rows.length){
+                res.json(200);
             }else{
                 res.json(500);
             }
-        });
+        }else{
+            res.json(500);
+        }
     });
 }
 
 module.exports.getProfileDetails = function (req, res){
     DAL.makeQuery({query: 'SELECT * from artists_profile where id = ?', escapedValues : [req.user[0].id]}, function(err, rows){
-        DAL.makeQuery({query: 'SELECT * from artists_languages where profile_id = ?', escapedValues : [rows[0].id]}, function(err, rowss){
-            console.log(rowss[0])
-            if (rowss[0]) {
-                rows[0].languages = rowss[0].language;
-            }
-            res.render('profile.dust', {user: rows[0], currentUser: req.user[0]});
-        });
+        if(!err){
+            res.render('profile.dust', {user: rows[0], currentUser: req.user[0]});  
+        }else{
+            res.json(500);
+        }
     });
 }
 
